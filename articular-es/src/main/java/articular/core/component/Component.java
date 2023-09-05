32cf22790b7a11815e173729e287568bfb8ea3dd
pavl_g <scrappers.tm@gmail.com>
2023-09-05 11:53:27 +0300

    Component.java: provided <I> input type to the game loop

--- articular-es/src/main/java/articular/core/component/Component.java
+++ articular-es/src/main/java/articular/core/component/Component.java
@@ -1,14 +1,46 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2023, Articular-ES, The AvrSandbox Project
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package articular.core.component;
 
 import articular.core.Entity;
-import articular.util.ArticulationManager;
+import articular.util.EntityComponentManager;
 
 /**
  * A template representing a Game {@link Entity} component.
  *
+ * @param <I> the input type to the game loop pattern
  * @author pavl_g
  */
-public interface Component {
+public interface Component<I> {
 
     /**
      * Retrieves the entity that encapsulates this component.
@@ -20,7 +52,7 @@ public interface Component {
      *
      * @return the entity encapsulating this game component
      */
-    Entity getEntity();
+    Entity<I> getEntity();
 
     /**
      * Updates this game component with an abstract input.
@@ -37,11 +69,11 @@ public interface Component {
      *
      * @param input an input to perform the update on
      */
-    void update(Object input);
+    void update(I input);
 
     /**
      * Retrieves the identifier (id) of this game component, the identifier is
-     * used by the {@link ArticulationManager} to link
+     * used by the {@link EntityComponentManager} to link
      * game components to their entities (aka. articulation).
      *
      * @return the component identifier object
@@ -69,19 +101,14 @@ public interface Component {
          *
          * @return the component identifier in longs
          */
-        public long getId() {
+        @Override
+        public long longValue() {
             return id;
         }
 
         @Deprecated
         @Override
         public int intValue() {
-            throw new UnsupportedOperationException("Deprecated call, use `Component.Id#getId()`");
-        }
-
-        @Deprecated
-        @Override
-        public long longValue() {
             throw new UnsupportedOperationException("Deprecated call, use `Component.Id#getId()`");
         }
 
