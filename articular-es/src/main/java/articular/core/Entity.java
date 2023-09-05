e3fd49b363c5c74932c584ff0f6c6150d34e0f12
pavl_g <scrappers.tm@gmail.com>
2023-09-05 11:51:25 +0300

    Entity.java: <I> game loop input type and inheritance from Component

--- articular-es/src/main/java/articular/core/Entity.java
+++ articular-es/src/main/java/articular/core/Entity.java
@@ -1,3 +1,34 @@
+/*
+* BSD 3-Clause License
+*
+* Copyright (c) 2023, Articular-ES, The AvrSandbox Project
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice, this
+*    list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+*    this list of conditions and the following disclaimer in the documentation
+*    and/or other materials provided with the distribution.
+*
+* 3. Neither the name of the copyright holder nor the names of its
+*    contributors may be used to endorse or promote products derived from
+*    this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package articular.core;
 
 import articular.core.component.Component;
@@ -7,12 +38,18 @@ import java.util.Map;
  * Represents a game entity with some {@link Component}s.
  *
  * <p>
+ * A game entity is considered also a game component object,
+ * this fact provides a pattern to implement sub-entities.
+ * </p>
+ *
+ * <p>
  * A Map of components must be provided via a DI pattern.
  * </p>
  *
+ * @param <I> the input type of the game loop
  * @author pavl_g
  */
-public interface Entity {
+public interface Entity<I> extends Component<I> {
 
     /**
      * A Map of game entity components to be provided.
@@ -23,7 +60,7 @@ public interface Entity {
      *
      * @return a map of components by their identifiers (not null)
      */
-    Map<? super Number, Component> getComponents();
+    Map<? super Number, Component<I>> getComponents();
 
     /**
      * Retrieves the name of this entity.
@@ -42,7 +79,7 @@ public interface Entity {
      * @param id the game entity component identifier
      * @return a game entity component or null if it doesn't exist (nullable)
      */
-    default Component getComponent(Component.Id id) {
+    default Component<I> getComponent(Component.Id id) {
         return getComponents().get(id);
     }
 
@@ -51,7 +88,7 @@ public interface Entity {
      *
      * @param input the input to the game loop pattern
      */
-    default void update(Object input) {
+    default void update(I input) {
         getComponents().forEach((id, component) -> component.update(input));
     }
 }
