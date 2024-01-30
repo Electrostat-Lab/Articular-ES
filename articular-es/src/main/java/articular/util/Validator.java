20cefcc3f90f9955b2b1ca7a3f80f02663551b09
pavl_g <scrappers.tm@gmail.com>
2024-01-30 03:26:11 +0300

    articular/util: applied intended architecture for utilities

--- articular-es/src/main/java/articular/util/Validator.java
+++ articular-es/src/main/java/articular/util/Validator.java
@@ -1,21 +1,60 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2024, Articular-ES, The AvrSandbox Project
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package articular.util;
 
-import articular.core.Entity;
-import articular.core.Type;
-import articular.core.system.AssociatedSystemNotFoundException;
-import articular.core.system.EntityNotFoundException;
-import articular.core.system.SystemController;
+import articular.throwable.AssociatedObjectNotFoundException;
 
 public final class Validator {
-    public static void validate(Type.EntityMap entityMap, SystemController systemController) {
-        if (entityMap == null) {
-            throw new AssociatedSystemNotFoundException(systemController);
+    public enum Message {
+        ASSOCIATED_SYSTEM_NOT_FOUND("System not found!"),
+        ASSOCIATED_ENTITY_COMPONENT_MAP_NOT_FOUND("Entity-Component map not found!"),
+        ASSOCIATED_SYSTEM_COMPONENT_MAP_NOT_FOUND("Cache is not found or disabled!"),
+        ENTITY_NOT_FOUND("Entity not found!"),
+        COMPONENT_NOT_FOUND("Component not found!");
+
+        private final String message;
+
+        Message(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
         }
     }
 
-    public static void validate(Type.ComponentMap componentMap, Entity entity) {
-        if (componentMap == null) {
-            throw new EntityNotFoundException(entity);
+    public static void validate(Validatable validatable, Message msg) {
+        if (validatable == null) {
+            throw new AssociatedObjectNotFoundException(msg);
         }
     }
 }
