92ed5be1871b611e5be835ab2e737b33954e8464
pavl_g <60224159+Scrappers-glitch@users.noreply.github.com>
2024-02-01 07:04:50 +0300

    articular-examples: main module and HID example (#13)
    * TestArticularManager: an HID example.

---
+++ articular-examples/src/main/java/articular/example/HID.java
@@ -0,0 +1,18 @@
+package articular.example;
+
+import articular.core.system.ArticularSystem;
+
+public enum HID implements ArticularSystem {
+    INPUT_PROCESSOR("InputData-processing"),
+    OUTPUT_PROCESSOR("OutputData-processing"),
+    FEATURE_PROCESSOR("FeatureData-processing");
+    private final String system;
+    HID(final String system) {
+        this.system = system;
+    }
+
+    @Override
+    public String getSystemName() {
+        return system;
+    }
+}
