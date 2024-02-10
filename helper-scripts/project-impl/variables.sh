e0bc2b6b6c688e44f9114da30ee8ceeeb0453091
pavl_g <scrappers.tm@gmail.com>
2024-02-10 03:06:44 +0300

    Articular-ES: testing and deployment scripting

---
+++ helper-scripts/project-impl/variables.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Maven sonatype stuff
+# ---------------------
+sonatype_url="https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+repository="ossrh"
+groupId="io.github.software-hardware-codesign"
+maven_version="3.9.3"
+maven_bin="./apache-maven-$maven_version/bin/mvn"
+
+lib_pomFile="./helper-scripts/project-impl/publishing/articular.pom"
+
+passphrase="avrsandbox"
+
+lib_artifactId_release="articular-es"
+
+settings="./helper-scripts/project-impl/publishing/maven-settings.xml"
+lib_build_dir="./articular-es/build/libs"
+# ---------------------
