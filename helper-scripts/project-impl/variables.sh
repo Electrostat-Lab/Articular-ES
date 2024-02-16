926ce868252235fc59df62d896010926c97b8ed0
pavl_g <60224159+Scrappers-glitch@users.noreply.github.com>
2024-02-16 18:36:35 +0300

    project-impl/variables.sh: updated maven binaries version with a valid one

--- helper-scripts/project-impl/variables.sh
+++ helper-scripts/project-impl/variables.sh
@@ -5,7 +5,7 @@
 sonatype_url="https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
 repository="ossrh"
 groupId="io.github.software-hardware-codesign"
-maven_version="3.9.3"
+maven_version="3.9.6"
 maven_bin="./apache-maven-$maven_version/bin/mvn"
 
 lib_pomFile="./helper-scripts/project-impl/publishing/articular.pom"
